2d2466cd66086ddfe30df0128c1361b6025a8824
antoniuskevin <kevin.wiguna@gmail.com>
2017-11-21 15:16:10 +0700

    tes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 import Image
 import ClbpS
 import ClbpM
@@ -5,7 +6,8 @@ import ClbpM
 if __name__ == '__main__':
     img = Image.Image("coba.jpg")
     img_read = img.run()
-
+    cv2.imshow("a", img_read)
+    cv2.waitKey()
     ClbpS_classifier = ClbpS.ClbpS(img_read)
     ClbpS_result = ClbpS_classifier.run()
 
