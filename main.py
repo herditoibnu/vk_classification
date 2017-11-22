78f1683ac73a99467c04d9cc2cb1bd49ad87a2ab
antoniuskevin <kevin.wiguna@gmail.com>
2017-11-22 14:02:06 +0700

    tambah knn

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
-import cv2
+import collections as cl
 import Image
 import ClbpS
 import ClbpM
 import ClbpC
 import numpy as np
 import os.path
-from PIL import Image as PImage
-from os import listdir
+
+
+
+def canberra_distance(testing, training):
+    kelas = training[514]
+    jarak = 0
+    i=0
+    for val in testing:
+        if val != 0 and testing[i] != 0:
+            jarak += (abs(val-training[i])/(abs(val)+abs(training[i])))
+    jarak_dan_kelas = [jarak, kelas]
+    return jarak_dan_kelas
+
+def akurasi(kelas_training, hasil):
+    total_benar = 0
+    count = 0
+    for val in kelas_training:
+        if val == hasil[count]:
+            total_benar+=1
+        count+=1
+    return (total_benar/count)*100
+
 
 if __name__ == '__main__':
-    dir_name = "kacamati/"
-   # inp = "F2.1.jpg"
 
-
-    # img = Image.Image("coba2.png")
-    file = open("datasit.txt","r")
+    dir_name = "d1/"
+    nilai_k = raw_input("Masukkan nilai k:")
+    file = open("dataset16.txt","r")
     kelas = 1
     flag = 0
+    hasil = []
+    kelas_testing = []
+
+    # mengubah gambar training menjadi histogram CLBP dan ditulis ke dalam file
     if not os.path.isfile("pixel.txt"):
         for item in file:
             flag+=1
@@ -42,20 +64,58 @@ if __name__ == '__main__':
             if flag == 16:
                 flag = 0
                 kelas +=1
-    # def loadImages(path):
-    #     # return array of images
-    #
-    #     imagesList = listdir(path)
-    #     loadedImages = []
-    #     for image in imagesList:
-    #         img = PImage.open(path + image)
-    #         loadedImages.append(img)
-    #         print img.filename
-    #     return loadedImages
-    #
-    #
-    # path = "DatasetWajah/"
-    #
-    # # your images in an array
-    # imgs = loadImages(path)
 
+    file = open("dataset4.txt", "r")
+    line_count = 0
+    flag = 0
+    kelas = 1
+
+    #iterasi file testing
+    for item in file:
+        flag+=1
+#        print item.strip()
+        img = Image.Image(dir_name + item.strip())
+        img_read = img.run()
+        # mencari histogram CLBP dari gambar input
+        ClbpS_classifier = ClbpS.ClbpS(img_read)
+        ClbpS_result = ClbpS_classifier.run()
+
+        ClbpM_classifier = ClbpM.ClbpM(img_read)
+        ClbpM_result = ClbpM_classifier.run()
+
+        ClbpC_classifier = ClbpC.ClbpC(img_read)
+        ClbpC_result = ClbpC_classifier.run()
+        list = np.concatenate((ClbpS_result, ClbpM_result, ClbpC_result), axis=0)
+        kelas_testing.append(kelas)
+        if flag == 4:
+            flag = 0
+            kelas += 1
+
+        file = open("pixel.txt","r")
+        line_count = 0
+        training_array = [] #array untuk menyimpan histogram CLBP dari file training, per image array direset
+        array_jarak = []
+
+        #menghitung jarak dari data testing ke setiap data training
+        for line in file:
+            line_count+=1
+            if line_count<=515:
+                training_array.append(line)
+            elif line_count == 516:
+                jarak = canberra_distance(list, training_array)
+                array_jarak.append(jarak)
+                line_count = 0
+                training_array = []
+
+        #mencari data jarak terkecil sebanyak k, mencari kelas yang paling banyak terjadi, dan ditaruh ke dalam array hasil
+        array_jarak.sort(key = lambda jarak: jarak[0])
+        i = 0
+        kelas_vote=[]
+        cnt = cl.Counter()
+        for jarak in array_jarak:
+            cnt[jarak[1]]+=1
+        hasil.append(max(cnt, key=cnt.get))
+
+    print kelas_testing
+    print hasil
+    print akurasi(kelas_testing,hasil)
